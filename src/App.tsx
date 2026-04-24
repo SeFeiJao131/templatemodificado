d8297c79d220d4173438f9294f5bdeb2136e3281
Rafael Mizobuchi <rafaelmizob@gmail.com>
2026-04-24 15:22:06 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <h1>Template modificado por mim</h1>
+  return (
+    <div>
+      <h1>Template modificado por mim</h1>
+      <h1>“Foi utilizado o template oficial do Vite com React como base, e foram realizadas modificações no código.”</h1>
+    </div>
+  )
 }
 
 export default App
